ed35f2eb8df2ad4afb8425d6b962aeee15bc4ad8
Paul-Longo <imanazarite88@gmail.com>
2021-09-13 11:30:26 -0400

    renamed HopperHero with all lowercase letters "hopperhero"

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# HopperHero
+# hopperhero
--- hopperhero/hopperhero/wsgi.py
+++ hopperhero/hopperhero/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HopperHero project.
+WSGI config for hopperhero project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HopperHero.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hopperhero.settings')
 
 application = get_wsgi_application()
