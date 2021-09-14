c7355fd14852af67d0082f9ae50f79c9bffa6e81
Paul-Longo <imanazarite88@gmail.com>
2021-09-14 16:25:06 -0400

    removed unnecessary import

--- hopperhero/authentication/urls.py
+++ hopperhero/authentication/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.serializers import as_serializer_error
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
 
