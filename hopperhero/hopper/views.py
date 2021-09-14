588fe5dad4ad9bab57ef513d8d11b828d07554e9
Paul-Longo <imanazarite88@gmail.com>
2021-09-14 15:47:56 -0400

    updated the views file in hopper app.

--- hopperhero/hopper/views.py
+++ hopperhero/hopper/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render
-from .models import Wod
-from .serializers import WodSerializer
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Wod
+from .serializers import WodSerializer
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-class WodList(APIView):
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_wod(request):
+    wod = Wod.objects.all()
+    serializer = WodSerializer(wod, many=True)
+    return Response(serializer.data)
 
-    def get(self, request):
-        wod = Wod.objects.all()
-        serializer = WodSerializer(wod, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
+@api_view(['POST', 'GET'])
+@permission_classes([IsAuthenticated])
+def user_wod(request):
+    if request.method == 'POST':
         serializer = WodSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            serializer.save(user=request.user)
+            return Response(status.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    elif request.method == "GET":
+        wod = Wod.objects.filter(user_id=request.user.id)
+        serializer = WodSerializer(wod, many=True)
+        return Response(serializer.data)
