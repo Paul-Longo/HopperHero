714490e9af0374c170eebf36983c245a1245d45d
Paul-Longo <imanazarite88@gmail.com>
2021-09-14 16:25:47 -0400

    added missing punctuation in class scope.

--- hopperhero/authentication/views.py
+++ hopperhero/authentication/views.py
@@ -6,5 +6,5 @@ from rest_framework.permissions import AllowAny
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = (AllowAny)
+    permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
