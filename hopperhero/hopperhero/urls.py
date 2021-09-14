dd8abd8e585d2a50889a52706c3ecc7e29d7568f
Paul-Longo <imanazarite88@gmail.com>
2021-09-14 15:49:08 -0400

    updated path in admin file

--- hopperhero/hopperhero/urls.py
+++ hopperhero/hopperhero/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path('', include('hopper.urls'))
+    path('api/hopper/', include('hopper.urls')),
 ]
