de1d83471003e9976252acb8eba912bec40575c8
Paul-Longo <imanazarite88@gmail.com>
2021-10-01 17:49:43 -0400

    added Likes class

--- hopperhero/hopper/models.py
+++ hopperhero/hopper/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Wod(models.Model):
-    users = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
 
+
 class Likes(models.Model):
-    users = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    like = models.ManyToManyField(Wod)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    wod = models.ForeignKey(Wod, on_delete=CASCADE)
