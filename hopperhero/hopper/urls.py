cde48dd643e574a2c0d422625c1c4f249a7f3ce6
Paul-Longo <imanazarite88@gmail.com>
2021-10-01 17:50:41 -0400

    added paths for likes, likes by id and user likes by id

--- hopperhero/hopper/urls.py
+++ hopperhero/hopper/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from hopper import views
+from .views import AllWods, LikesView, UserLikesView, WodsView
 
 urlpatterns = [
-    path('all/', views.get_all_wod),
-    path('<int:id>/', views.get_wod),
-    path('likes/<int:id>/', views.get_likes)
+    path('all/', AllWods.as_view()),
+    path('<int:id>/', WodsView.as_view()),
+    path('likes/', LikesView.as_view()),
+    path('likes/<int:id>/', LikesView.as_view()),
+    path('likes/user/<int:id>/', UserLikesView.as_view())
 ]
