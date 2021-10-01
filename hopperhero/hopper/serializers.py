3e104b346f44675f22593c5a22592dc8b0de2ec4
Paul-Longo <imanazarite88@gmail.com>
2021-10-01 17:50:15 -0400

    added LikesSerializer

--- hopperhero/hopper/serializers.py
+++ hopperhero/hopper/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Wod
+from .models import Wod, Likes
 
 class WodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wod
         fields = ['id', 'name', 'description']
+
+class LikesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Likes
+        fields = ['id', 'user', 'wod']
